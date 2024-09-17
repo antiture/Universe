766b18b620fd69756b081fc819f975fbf6662d16
Zijian Xi <antiture@gmail.com>
2024-09-17 12:04:21 +0200

    init

---
+++ Universe.Horizon/Universe.Horizon/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Universe.Horizon
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Universe.Horizon/Universe.Horizon/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Universe.Horizon
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
